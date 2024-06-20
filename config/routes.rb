1e83472451ff7869126a6cb250653cfab2d4ad2d
Francis ABODO <abodojustin@gmail.com>
2024-06-20 16:01:35 +0100

    Formulaire d'Inscription

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'terms', to: 'pages#terms'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
