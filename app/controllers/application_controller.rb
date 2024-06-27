c1e091b04bf334b8af60f606ab6d826e60ac18bd
Francis ABODO <abodojustin@gmail.com>
2024-06-27 06:17:41 +0100

    Accessing the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+    
+    def set_current_user
+        if session[:user_id]
+            @user = User.find_by(id: session[:user_id])
+            flash[:notice] = "Connecté avec succès"
+        end
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,6 @@ class PagesController < ApplicationController
     end
 
     def home
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-            flash[:notice] = "Connecté avec succès"
-        # else
-        #     flash[:alert] = "Email ou mot de passe invalide"
-        #     redirect_to sign_up, notice: "Email ou mot de passe invalide"
-        end
     end
 
     def about
