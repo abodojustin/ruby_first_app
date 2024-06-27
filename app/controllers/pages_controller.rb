bfc5028c6b1a836a770f8582ac3a562c6c0c4ea3
Francis ABODO <abodojustin@gmail.com>
2024-06-27 05:41:11 +0100

    logout users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,9 @@ class PagesController < ApplicationController
         if session[:user_id]
             @user = User.find_by(id: session[:user_id])
             flash[:notice] = "Connecté avec succès"
-        else
-            flash[:alert] = "email ou mot de passe invalide"
-            redirect_to sign_up
+        # else
+        #     flash[:alert] = "Email ou mot de passe invalide"
+        #     redirect_to sign_up, notice: "Email ou mot de passe invalide"
         end
     end
 
